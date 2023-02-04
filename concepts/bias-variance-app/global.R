127cacaf3077becff7b9e01e03103354a5ff6549
Janos Divenyi <janos.divenyi@emarsys.com>
2023-02-04 22:13:27 +0100

    add updated bias-variance-app

---
+++ concepts/bias-variance-app/global.R
@@ -0,0 +1,31 @@
+library(tibble)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(shinyjs)
+theme_set(theme_bw(base_size = 16))
+
+f_y_x <- function(x) {
+    8 * x^5 - x^4 - 10 * x^3 - 0.5 * x^2 + 4 * x
+}
+
+default_n <- 100
+default_sd <- 0.5
+x0 <- 0.75
+
+generate_data <- function(n, sd) {
+    x <- runif(n)
+    tibble(x = x, y = f_y_x(x) + rnorm(n, sd = sd))
+}
+
+calculate_mse <- function(x) {
+    mean(x)^2 + var(x)
+}
+
+linear_model <- function(data) {
+    lm(y ~ x, data)
+}
+
+quintic_model <- function(data) {
+    lm(y ~ x + I(x^2) + I(x^3) + I(x^4) + I(x^5), data)
+}
---
+++ concepts/bias-variance-app/ui.R
@@ -0,0 +1,37 @@
+fluidPage(
+    shinyjs::useShinyjs(),
+    titlePanel("Bias-Variance trade-off"),
+
+    sidebarLayout(
+        sidebarPanel(
+            withMathJax("Our true model is $$Y = \\beta_1 X^5 + \\beta_2 X^4 + ... + \\beta_5 X + \\varepsilon$$"),
+            withMathJax("where $$\\varepsilon \\sim \\mathbf{N}(0, \\sigma)$$"),
+            br(),
+            HTML("We estimate two polynomials to predict <I>Y</I>:
+                 one of degree 5 (true model) and one of degree 1 (simple linear)."),
+            br(),
+            br(),
+            HTML("We evaluate our estimators on a new data point: <I>X=0.75</I>.
+                 The resulting errors are shown as dots on a second plot.
+                 Thick line segments depict their averages (the bias).
+                 The calculated MSE values are added as labels."),
+            br(),
+            br(),
+            radioButtons("n", "Number of observations", choices = c(100, 1000), selected = default_n),
+            radioButtons("error_sd", withMathJax("Standard deviation of noise (sigma)"), choices = c(0.5, 5), selected = default_sd),
+            actionButton("run", "Simulate"),
+            actionButton("run100", "Simulate 100x"),
+            br(),
+            hr(style = "border-top: 1px solid #909090;"),
+            HTML("<B>Lesson:</B> You can realize that estimating the wrong model might result in a better predictor
+                 than estimating the true one if the noise is relatively large (few observations, large variance of noise). 
+                 This is because the wrong model is a simpler one
+                 that is easier to estimate, so its prediction variance is going to be much smaller. 
+                 This could compensate for not being able to predict the true value on average (bias).")
+        ),
+        mainPanel(
+            plotOutput("xy_plot", height = 370),
+            plotOutput("error_plot", height = 370)
+        )
+    )
+)
